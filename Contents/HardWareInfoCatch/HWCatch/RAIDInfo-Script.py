436c2fc2fae1fe1948e39af0098d8e18fd125dda
KKtheGhost <kivinsae@gmail.com>
2018-11-12 12:39:03 +0800

    updt

---
+++ Contents/HardWareInfoCatch/HWCatch/RAIDInfo-Script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#@Date    : 2018-11-06 16:43:06
+#@Author  : Kivinsae Fang (fangwei@xindong.com)
+#@QQ      : 1669815117
+#@Version : 0.1.0
+
+import paramiko
+import metrics
+import os
+import re
+
