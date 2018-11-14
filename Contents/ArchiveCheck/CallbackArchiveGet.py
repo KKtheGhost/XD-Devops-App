fe6684a153c66dcc0ce503a9ec21f01c841ee032
KKtheGhost <kivinsae@gmail.com>
2018-11-14 12:42:05 +0800

    部分优化

--- Contents/ArchiveCheck/CallbackArchiveGet.py
+++ Contents/ArchiveCheck/CallbackArchiveGet.py
@@ -7,30 +7,14 @@
 
 import paramiko
 import time
-import datetime
 import os
-import sys
+import sys 
+import datetime
 
 reload(sys)
 sys.setdefaultencoding('utf-8') 
 
-timespit = str(1000000000 * int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d'))))
-
-jk002ip,jk002username = '10.1.52.1','root'
-
-cmdline = 0
-sshcommand = ''
-
-## def INFCommand(cmd):
-##     cmdfile = open('./jk002INFCMD','r')
-##     projcmd = cmdfile.readlines()[cmdline]
-##     for i in projcmd.strip():
-##         cmd += i
-
 def jk002SSH(ip,username,cmd,stdoutfile):
-    ## callbackLogFile = open("./CallBack_daily_log","a")
-    ## callbackLogFile.seek(0)
-    ## callbackLogFile.truncate()
     try:
         ssh_client = paramiko.SSHClient()
         ssh_client.load_system_host_keys()
@@ -38,8 +22,6 @@ def jk002SSH(ip,username,cmd,stdoutfile):
         pkey = paramiko.RSAKey.from_private_key_file('/Users/kivinsaefang/.ssh/id_rsa')         ##设定SSH免密登录
         ssh_client.connect(ip,22,username,pkey=pkey)                                            ##设定ssh_client的登录信息
         stdin,stdout,stderr = ssh_client.exec_command(cmd,get_pty=True)                  ##运行需要的查询指令
-        ##在command命令最后加上 get_pty=True，执行多条命令 的话用；隔开，另外所有命令都在一个大的单引号范围内引用
-        ##std_in.write('PWD'+'\n') #执行输入命令，输入sudo命令的密码，会自动执行
         for line in stdout:
             line = line.strip() ##删除换行符，防止出现空行
             if 'archive' in line:
@@ -49,51 +31,50 @@ def jk002SSH(ip,username,cmd,stdoutfile):
             elif '----' in line:
                 continue
             else:
-                char = line[20:]         ##变量分离
-                print >> stdoutfile,char   ##输出到文件："./CallBack_daily_log"
-            ## callbackLogFile.write(stdout.read())
-        ssh_client.close()                      ##完成操作，关闭ssh_client
+                char = line[20:]
+                print >> stdoutfile,char 
+        ssh_client.close()
     except Exception,e:
         print e
 
-def CallbackOut(num,cmd,ip,username,rplcment):    
+def CompareError(project):
+    ArchiveStatus = 0
+    TodayLog = open("./CallBack_ArchiveDailyLog","r")
+    StandardLog = open("./STD_ArchiveLog","r")
+    TodayLogLines = TodayLog.readlines()
+    StandardLogLines = StandardLog.readlines()
+    StandardLogProj,ErrorLine = [],[]
+    for stdline in StandardLogLines:             ##不可优化部分，STDBackupsout存在随业务变化的可能。
+        StandardLogProj += stdline
+    for line in TodayLogLines:
+        if line.strip() not in StandardLogProj:
+            ErrorLine += line.strip()[15:((line.strip()[15:]).append(' '))]     ##通过append获取rdsname
+            ArchiveStatus += 1
+    if ArchiveStatus == 0:
+        print project + '\'s archive is perfect today.'
+    else:
+        print 'There is ' + str(ArchiveStatus) + ' errors of ' + project + ' in Archive today. The error RDSserver is ' + ErrorLine 
+
+def CallbackOut(num,cmd,ip,username): 
+    timespit = str(1000000000 * int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d'))))   
     callbackLogFile = open("./CallBack_ArchiveDailyLog","a")
     callbackLogFile.seek(0)
     callbackLogFile.truncate()
     while num < 14:
         cmdfile = open('./jk002_ArchiveCmd','r')
         projcmd = cmdfile.readlines()[num]
-        projcmd = projcmd.replace('timespit',rplcment)
+        projcmd = projcmd.replace('timespit',timespit)
         for i in projcmd.strip():
             cmd += i
-        ## print cmd
         jk002SSH(ip,username,cmd,callbackLogFile)
+        project = ((open('../ProjList','r').readlines()[num]).strip())[10:]
         num += 1
         cmd = ''
-        ## print 'Stdout OK!'
+        CompareError(project)
     print 'Done!'
 
-def CompareError():
-    ArchiveStatus = 1
-    TodayLog = open("./CallBack_ArchiveDailyLog","r")
-    StandardLog = open("./STD_ArchiveLog","r")
-    TodayLogLines = TodayLog.readlines()
-    StandardLogLines = StandardLog.readlines()
-    for stdline in StandardLogLines:
-        res = 0
-        for line in TodayLogLines:
-            if line == stdline:
-                res += 1
-            else:
-                continue
-        if res == 0:
-            print stdline
-            ArchiveStatus += 1
-    if ArchiveStatus == 1:
-        print 'Archive Status OK!'
-    else:
-        print 'Errors are listed above.'
+jk002ip,jk002username = '10.1.52.1','root'
+cmdline = 0
+sshcommand = ''
 
-        
-CallbackOut(cmdline,sshcommand,jk002ip,jk002username,timespit)
-CompareError()
+CallbackOut(cmdline,sshcommand,jk002ip,jk002username)
--- Contents/BackupCheck/CallbackBackupGet.py
+++ Contents/BackupCheck/CallbackBackupGet.py
@@ -14,16 +14,7 @@ import datetime
 reload(sys)
 sys.setdefaultencoding('utf-8') 
 
-## def INFCommand(cmd):
-##     cmdfile = open('./jk002INFCMD','r')
-##     projcmd = cmdfile.readlines()[cmdline]
-##     for i in projcmd.strip():
-##         cmd += i
-
 def jk002SSH(ip,username,cmd,stdoutfile):
-    ## callbackLogFile = open("./CallBack_daily_log","a")
-    ## callbackLogFile.seek(0)
-    ## callbackLogFile.truncate()
     try:
         ssh_client = paramiko.SSHClient()
         ssh_client.load_system_host_keys()
@@ -31,9 +22,6 @@ def jk002SSH(ip,username,cmd,stdoutfile):
         pkey = paramiko.RSAKey.from_private_key_file('/Users/kivinsaefang/.ssh/id_rsa')         ##设定SSH免密登录
         ssh_client.connect(ip,22,username,pkey=pkey)                                            ##设定ssh_client的登录信息
         stdin,stdout,stderr = ssh_client.exec_command(cmd,get_pty=True)                  ##运行需要的查询指令
-        ##在command命令最后加上 get_pty=True，执行多条命令 的话用；隔开，另外所有命令都在一个大的单引号范围内引用
-        ##std_in.write('PWD'+'\n') #执行输入命令，输入sudo命令的密码，会自动执行
-        ##stdout.encode('gb2312')
         for line in stdout:
             line = line.strip() 
             if 'backup' in line:
@@ -55,40 +43,38 @@ def CompareError(project):
     StandardLog = open("./STD_BackupLog","r")
     TodayLogLines = TodayLog.readlines()
     StandardLogLines = StandardLog.readlines()
-    StandardLogProj = []
-    for stdline in StandardLogLines:             ##可优化部分
+    StandardLogProj,ErrorLine = [],[]
+    for stdline in StandardLogLines:             ##不可优化部分，STDBackupsout存在随业务变化的可能。
         StandardLogProj += stdline
     for line in TodayLogLines:
         if line.strip() not in StandardLogProj:
-            print line
+            ErrorLine += line.strip()[15:((line.strip()[15:]).append(' '))]     ##通过append获取rdsname
             BackupStatus += 1
     if BackupStatus == 0:
         print project + '\'s backup is perfect today.'
     else:
-        print 'There is ' + str(BackupStatus) + ' errors of ' + project + ' in Backup today '
+        print 'There is ' + str(BackupStatus) + ' errors of ' + project + ' in Backup today. The error RDSserver is ' + ErrorLine 
 
-def CallbackOut(num,cmd,ip,username,rplcment):    
+def CallbackOut(num,cmd,ip,username):
+    timespit = str(1000000000 * int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d'))))     
     callbackLogFile = open("./CallBack_BackupDailyLog","a")
     callbackLogFile.seek(0)
     callbackLogFile.truncate()
     while num < 14:
         cmdfile = open('./jk002_BackupCmd','r')
         projcmd = cmdfile.readlines()[num]
-        projcmd = projcmd.replace('timespit',rplcment)
+        projcmd = projcmd.replace('timespit',timespit)
         for i in projcmd.strip():
             cmd += i
-        ## print cmd
         jk002SSH(ip,username,cmd,callbackLogFile)
-        project = ((open('../ProjList','r').readlines()[num]).strip())[12:]
+        project = ((open('../ProjList','r').readlines()[num]).strip())[10:]
         num += 1
         cmd = ''
         CompareError(project)
-        ## print 'Stdout OK!'
     print 'Done!'
 
-timespit = str(1000000000 * int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d'))))
 jk002ip,jk002username = '10.1.52.1','root'
 cmdline = 0
 sshcommand = ''
 
-CallbackOut(cmdline,sshcommand,jk002ip,jk002username,timespit)
+CallbackOut(cmdline,sshcommand,jk002ip,jk002username)
