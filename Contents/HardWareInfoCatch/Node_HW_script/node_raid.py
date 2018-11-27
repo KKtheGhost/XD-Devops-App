064552cbddb0fc697995a58b501b16ece44c55eb
KKtheGhost <kivinsae@gmail.com>
2018-11-27 18:07:57 +0800

    HP raid信息抓取更新

--- Contents/HardWareInfoCatch/Node_HW_script/node_raid.py
+++ Contents/HardWareInfoCatch/Node_HW_script/node_raid.py
@@ -34,14 +34,17 @@ def get_server_raid_card_metrics():
                 influx_raid_record_fields["raid_health"] = 1
             ## "nvme_health"对于有无nvme固态判断，并输出有无报错
             nvme_get_mounted_route_command = 'lsblk|grep nvme|sed \'1d\'|awk \'{print $7}\''
-            nvme_mounted_route = commands.getoutput(nvme_get_mounted_route_command)     
-            nvme_status = commands.getoutput('ls ' + nvme_mounted_route)
-            if nvme_status == 'ls:' or '-ba':
+            nvme_mounted_route = commands.getoutput(nvme_get_mounted_route_command)
+            if nvme_mounted_route == '':
                 influx_raid_record_fields["nvme_health"] = 1
-            elif nvme_status == '':
-                influx_raid_record_fields["nvme_health"] = 0
             else:
-                influx_raid_record_fields["nvme_health"] = 1
+                nvme_status_get = commands.getoutput('ls ' + nvme_mounted_route)
+                if nvme_status_get == 'ls:' or '-ba':
+                    influx_raid_record_fields["nvme_health"] = 1
+                elif nvme_status_get == '':
+                    influx_raid_record_fields["nvme_health"] = 0
+                else:
+                    influx_raid_record_fields["nvme_health"] = 1
             ## "physical_disk_health"通过raid卡指令判断机械硬盘状态的部分
             dell_raid_info_get_command = "/opt/MegaRAID/MegaCli/MegaCli64 -pdlist -a0|grep -iE 'slot|Non Coerced Size|firmware state|Error'"
             dell_raid_info = commands.getoutput(dell_raid_info_get_command)
@@ -59,7 +62,7 @@ def get_server_raid_card_metrics():
                     if (dell_raid_info_list[dell_raid_info_list_index + 1].split( ))[3] != '0' and (dell_raid_info_list[dell_raid_info_list_index].split( ))[0] == 'Other':
                         error_slot_state.join('和接触连接错误')
                 else:
-                    influx_raid_record_fields["physical_disk_health"] = 0
+                    influx_raid_record_fields["physical_disk_health"] = 1
                     error_slot_state = 'OK'
             return influx_raid_record_fields,error_slot_state
         ## 当型号是R610的时候==========================================
@@ -85,13 +88,16 @@ def get_server_raid_card_metrics():
         ## "nvme_health"对于有无nvme固态判断，并输出有无报错
         nvme_get_mounted_route_command = 'lsblk|grep nvme|sed \'1d\'|awk \'{print $7}\''
         nvme_mounted_route = commands.getoutput(nvme_get_mounted_route_command)
-        nvme_status = commands.getoutput('ls ' + nvme_mounted_route)
-        if nvme_status == 'ls:' or '-ba':
+        if nvme_mounted_route == '':
             influx_raid_record_fields["nvme_health"] = 1
-        elif nvme_status == '':
-            influx_raid_record_fields["nvme_health"] = 0
         else:
-            influx_raid_record_fields["nvme_health"] = 1
+            nvme_status_get = commands.getoutput('ls ' + nvme_mounted_route)
+            if nvme_status_get == 'ls:' or '-ba':
+                influx_raid_record_fields["nvme_health"] = 1
+            elif nvme_status_get == '':
+                influx_raid_record_fields["nvme_health"] = 0
+            else:
+                influx_raid_record_fields["nvme_health"] = 1
         ## "physical_disk_health"通过raid卡指令判断机械硬盘状态的部分
         huawei_raid_info_get_command = "/opt/MegaRAID/MegaCli/MegaCli64 -pdlist -a0|grep -iE 'slot|Non Coerced Size|firmware state|Error'"
         huawei_raid_info = commands.getoutput(huawei_raid_info_get_command)
@@ -105,7 +111,7 @@ def get_server_raid_card_metrics():
                 if (huawei_raid_info_list[huawei_raid_info_list_index + 1].split( ))[3] != '0' and (huawei_raid_info_list[huawei_raid_info_list_index].split( ))[0] == 'Other':
                     error_slot_state.join('和接触连接错误')
             else:
-                influx_raid_record_fields["physical_disk_health"] = 0
+                influx_raid_record_fields["physical_disk_health"] = 1
                 error_slot_state = 'OK'
         return influx_raid_record_fields,error_slot_state
 
@@ -123,106 +129,48 @@ def get_server_raid_card_metrics():
         ## "nvme_health"对于有无nvme固态判断，并输出有无报错
         nvme_get_mounted_route_command = 'lsblk|grep nvme|sed \'1d\'|awk \'{print $7}\''
         nvme_mounted_route = commands.getoutput(nvme_get_mounted_route_command)
-        nvme_status_get = commands.getoutput('ls ' + nvme_mounted_route)
-        if nvme_status == 'ls:' or '-ba':
+        if nvme_mounted_route == '':
             influx_raid_record_fields["nvme_health"] = 1
-        elif nvme_status == '':
-            influx_raid_record_fields["nvme_health"] = 0
         else:
-            influx_raid_record_fields["nvme_health"] = 1
+            nvme_status_get = commands.getoutput('ls ' + nvme_mounted_route)
+            if nvme_status_get == 'ls:' or '-ba':
+                influx_raid_record_fields["nvme_health"] = 1
+            elif nvme_status_get == '':
+                influx_raid_record_fields["nvme_health"] = 0
+            else:
+                influx_raid_record_fields["nvme_health"] = 1
         ## "physical_disk_health"通过raid卡指令判断机械硬盘状态的部分
-        hp_raid_info_get_command = "/opt/MegaRAID/MegaCli/MegaCli64 -pdlist -a0|grep -iE 'slot|Non Coerced Size|firmware state|Error'"
-        hp_raid_info = commands.getoutput(hp_raid_info_get_command)
-        hp_raid_info_list = hp_raid_info.split('\n')
-        hp_raid_info_list_index = 1
-        
+        hp_product_check_command = 'dmidecode -t 1 | grep Product|awk \'{print $3,$4,$5}\''
+        hp_product = commands.getoutput(hp_product_check_command)
+        if hp_product == 'ProLiant DL360p Gen8':
+            hp_raid_info_get_command = "hpssacli ctrl slot=0 pd all show|grep physicaldrive"
+            hp_raid_info = commands.getoutput(hp_raid_info_get_command)
+            hp_raid_info_list = hp_raid_info.split('\n')
+            hp_raid_info_list_index = 0
+            while hp_raid_info_list_index < len(hp_raid_info_list):
+                if (hp_raid_info_list[hp_raid_info_list_index]).split( )[9] != 'OK)':
+                    hp_error_slot_number = ((hp_raid_info_list[hp_raid_info_list_index]).split( )[4] + ' ' + (hp_raid_info_list[hp_raid_info_list_index]).split( )[5])[2:]
+                    hp_error_slot_size = ((hp_raid_info_list[hp_raid_info_list_index]).split( )[7] + ' ' + ((hp_raid_info_list[hp_raid_info_list_index]).split( )[8])[:2])
+                    influx_raid_record_fields["physical_disk_health"] = 0
+                    error_slot_state = hp_error_slot_number + ' 存在扇区错误,' + ' 磁盘容量为' + hp_error_slot_size + '.'
+                else:
+                    influx_raid_record_fields["physical_disk_health"] = 1
+                    error_slot_state = 'OK'
+            return influx_raid_record_fields,error_slot_state
+        elif hp_product == 'ProLiant DL380 Gen9':
+            hp_raid_info_get_command = "hpssacli ctrl slot=0 pd all show|grep physicaldrive"
+            hp_raid_info = commands.getoutput(hp_raid_info_get_command)
+            hp_raid_info_list = hp_raid_info.split('\n')
+            hp_raid_info_list_index = 0
+            while hp_raid_info_list_index < len(hp_raid_info_list):
+                if (hp_raid_info_list[hp_raid_info_list_index]).split( )[9] != 'OK)':
+                    hp_error_slot_number = ((hp_raid_info_list[hp_raid_info_list_index]).split( )[4] + ' ' + (hp_raid_info_list[hp_raid_info_list_index]).split( )[5])[2:]
+                    hp_error_slot_size = ((hp_raid_info_list[hp_raid_info_list_index]).split( )[7] + ' ' + ((hp_raid_info_list[hp_raid_info_list_index]).split( )[8])[:2])
+                    influx_raid_record_fields["physical_disk_health"] = 0
+                    error_slot_state = hp_error_slot_number + ' 存在扇区错误,' + ' 磁盘容量为' + hp_error_slot_size + '.'
+                else:
+                    influx_raid_record_fields["physical_disk_health"] = 1
+                    error_slot_state = 'OK'
+            return influx_raid_record_fields,error_slot_state
 
-
-
-        return influx_raid_record_fields,error_slot_state
-
-##========================================================================================
-##========================================================================================
-
-def get_dell_raid_card_metrics():
-    command = "/opt/MegaRAID/perccli/perccli64 /c0/bbu show |grep -A 2 -i 'Ctrl' |tail -1"
-    output = commands.getoutput(command)
-    fields = {"health": 1}
-    tags = {}
-    if "use /cx/cv" in output:
-        command = "/opt/MegaRAID/perccli/perccli64 /c0/cv show|grep -E -i 'Model|Ctrl' -A 2 |tail -1"
-        output = commands.getoutput(command)
-        if "Optimal" in output:
-            fields["health"] = 1
-        else:
-            fields["health"] = 0
-    elif "Battery is absent!" in output:
-        fields["health"] = 1
-        tags["status"] = output
-
-    no_battery_model_list = ['H310', 'H330', 'HBA330']
-    model_command = "/opt/MegaRAID/perccli/perccli64 /c0 show | grep 'Product Name'"
-    model_output = commands.getoutput(model_command)
-
-    for model in no_battery_model_list:
-        if model in model_output:
-            fields["health"] = 1
-            tags["status"] = 'This raid card do not have battery!'
-
-    metrics = Metrics()
-    metrics.add(Metric("hardware.raid_card_battery", tags, dict_to_fields(fields)))
-    return metrics.to_string()
-
-
-def get_slotnum():
-    command = "/usr/sbin/hpssacli ctrl all show config"
-    slot = commands.getoutput(command)
-
-    pslot = re.compile(r'(\w+ Slot) +(?P<slot>[0-9]+) +(.*)')
-    mslot = pslot.search(slot)
-    slotnum = mslot.group("slot")
-    return slotnum
-
-
-def get_hp_raid_card_metrics(): ## 惠普Raid卡信息获取
-    slotnum = get_slotnum()
-    battery_command = "/usr/sbin/hpssacli ctrl slot={0} show | grep 'Battery/Capacitor Status:'".format(slotnum)
-    cache_command = "/usr/sbin/hpssacli ctrl slot={0} show | grep 'Cache Status:'".format(slotnum)
-
-    battery_result = commands.getoutput(battery_command)
-    if ": " in battery_result:
-        battery_result = battery_result.split(": ")[1]
-    else:
-        battery_result = "No battery status"
-
-    cache_result = commands.getoutput(cache_command).split(": ")[1]
-    fields = {}
-    tags = {}
-    if "OK" in cache_result:
-        if "Recharging" in battery_result or "OK" in battery_result:
-            fields["health"] = 1
-            tags["status"] = cache_result
-        else:
-            fields["health"] = 0
-            tags["status"] = "Battery: " + battery_result
-    else:
-        fields["health"] = 0
-        tags["status"] = "Cache: " + cache_result
-
-    metrics = Metrics()
-    metrics.add(Metric("hardware.raid_card_battery", tags, dict_to_fields(fields)))
-    return metrics.to_string()
-
-
-if __name__ == '__main__':
-    output = commands.getoutput("dmidecode -t 1")
-    if "Dell" in output:
-        print get_dell_raid_card_metrics()
-    elif "HP" in output:
-        print get_hp_raid_card_metrics()
-    elif "Huawei" in output:
-        print get_huawei_raid_card_metrics()
-    elif "Inspur" in output:
-        print get_huawei_raid_card_metrics()
-    else:
-        exit(1)
+get_server_raid_card_metrics()
